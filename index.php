5c9581a2027e3dba632bbd7b030c367b30b6a5aa
Warley Rodrigues <warleyhacker@hotmail.com>
2022-01-23 11:09:02 -0300

    2.0

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://code.jquery.com/jquery-3.6.0.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
     <link rel="stylesheet" href="style.css">    
     <title>PHP sem refresh</title>
 </head>
